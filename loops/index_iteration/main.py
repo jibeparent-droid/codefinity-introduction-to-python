dfc46381adf95c0b8aa5e506f91c0e242e83f0f5
jibeparent-droid <>
2025-12-13 13:13:14 +0000

    Index Iteration

---
+++ loops/index_iteration/main.py
@@ -0,0 +1,18 @@
+prices = [29.99, 45.50, 12.75, 38.20]
+discounts = [0.10,0.20,0.15,0.05]
+#updated_price =[] # méthode liste
+for i in range(len((prices))):
+    prices[i] *= (1-discounts[i])
+    print (f"Updated price for item {i}: §{prices[i]:.2f}")
+    
+
+    
+    ''' la méthode liste  avec "updated_price" estbonne mais la validation se fait sur
+    la liste "prices" qui ne change pas'''
+    #updated_price.append(prices[i]*(1-discounts[i]))
+    #print (f"Updated price for item {i}: {updated_price[i]:.2f}")
+    
+    
+    
+
+    
